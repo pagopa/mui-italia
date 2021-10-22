a4c5a3d7bda1f756a75f5b75e4f57d34020d7085
Vito Falco <vitofalco@gmail.com>
2021-10-22 12:36:46 +0200

    Init project with some base stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+import Buttons from "./componentsexample/Buttons";
+import Types from "./componentsexample/Typography";
+import Tabs from "./componentsexample/Tabs";
+import Divider from "@mui/material/Divider";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Box m={5} sx={{ width: "100%", maxWidth: 960 }}>
+          <Typography variant="h1" component="div" gutterBottom>
+            Welcome, this is the MUI Italia theme.
+          </Typography>
+          <Typography variant="h4" gutterBottom component="div">
+            An experimental theme MUI based, inspired by{" "}
+            <Link href="https://italia.github.io/bootstrap-italia/">
+              Bootstrap Italia
+            </Link>
+            .
+          </Typography>
+          <Typography my={3} variant="h6" component="div" gutterBottom>
+            Examples of Buttons
+          </Typography>
+          <Buttons></Buttons>
+          <Divider />
+          <Typography my={3} variant="h6" component="div" gutterBottom>
+            Examples of Heading and typography
+          </Typography>
+          <Types></Types>
+          <Divider />
+          <Typography my={3} variant="h6" component="div" gutterBottom>
+            Examples of Tabs
+          </Typography>
+          <Tabs></Tabs>
+        </Box>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/componentsexample/Buttons.tsx
@@ -0,0 +1,66 @@
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+export default function Buttons() {
+  return (
+    <>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Primary</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+      <Stack direction="row" spacing={2} my={2}>
+        <Button>Primary</Button>
+        <Button disabled>Disabled</Button>
+        <Button href="#text-buttons">Link</Button>
+      </Stack>
+      <Stack direction="row" spacing={2} my={2}>
+        <Button variant="outlined">Primary</Button>
+        <Button variant="outlined" disabled>
+          Disabled
+        </Button>
+        <Button variant="outlined" href="#outlined-buttons">
+          Link
+        </Button>
+      </Stack>
+      <Stack direction="row" spacing={2} my={2}>
+        <Button color="secondary">Secondary</Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+        <Button variant="outlined" color="error">
+          Error
+        </Button>
+      </Stack>
+      <Stack sx={{ "& button": { m: 1 } }}>
+        <div>
+          <Button size="small">Small</Button>
+          <Button size="medium">Medium</Button>
+          <Button size="large">Large</Button>
+        </div>
+        <div>
+          <Button variant="outlined" size="small">
+            Small
+          </Button>
+          <Button variant="outlined" size="medium">
+            Medium
+          </Button>
+          <Button variant="outlined" size="large">
+            Large
+          </Button>
+        </div>
+        <div>
+          <Button variant="contained" size="small">
+            Small
+          </Button>
+          <Button variant="contained" size="medium">
+            Medium
+          </Button>
+          <Button variant="contained" size="large">
+            Large
+          </Button>
+        </div>
+      </Stack>
+    </>
+  );
+}
