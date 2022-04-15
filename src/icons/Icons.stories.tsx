cd6f1079f4ed5d5acb5f0528de06db4308432c93
Damiano Plebani <damiano.plebani@pagopa.it>
2022-04-15 09:44:12 +0200

    [MUI-26] Add Spid & Cie icons

---
+++ src/icons/Icons.stories.tsx
@@ -0,0 +1,58 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Stack, Box, Typography } from "@mui/material";
+
+import { MediumIcon, SpidIcon, CieIcon } from "./";
+
+export interface IconBoxProps {
+  icon: JSX.Element;
+  name: string;
+}
+
+export default {
+  title: "Assets/Icons",
+} as ComponentMeta<typeof Box>;
+
+const IconBox = ({ name, icon }: IconBoxProps): JSX.Element => (
+  <Stack
+    spacing={2}
+    sx={{
+      backgroundColor: "background.paper",
+      borderRadius: 2,
+      p: 1,
+      pt: 2,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    }}
+  >
+    {icon}
+    {name && <Typography sx={{ fontSize: 12 }}>{name}</Typography>}
+  </Stack>
+);
+
+export const Overview: ComponentStory<typeof Box> = () => (
+  <Box
+    sx={{
+      display: "grid",
+      gridTemplateColumns: "repeat(auto-fill, minmax(100px, 1fr))",
+      gap: 2,
+    }}
+  >
+    <IconBox name={"MediumIcon"} icon={<MediumIcon />} />
+    <IconBox name={"SpidIcon"} icon={<SpidIcon />} />
+    <IconBox name={"CieIcon"} icon={<CieIcon />} />
+  </Box>
+);
+
+Overview.parameters = {
+  controls: { hideNoControlsWarning: true },
+};
+
+Overview.decorators = [
+  (Story) => (
+    <div style={{ padding: "16px", backgroundColor: "#F5F5F5" }}>
+      <Story />
+    </div>
+  ),
+];
