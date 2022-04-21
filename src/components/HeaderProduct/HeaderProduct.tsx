cd31ff574063afdfc5196d1c0eb562b03bca45db
Damiano Plebani <hello@damianoplebani.com>
2022-04-21 18:42:25 +0200

    Fix `HeaderProduct` wrong info display when PartyItem is visible in desktop state

--- src/components/HeaderProduct/HeaderProduct.tsx
+++ src/components/HeaderProduct/HeaderProduct.tsx
@@ -97,7 +97,7 @@ export const HeaderProduct = ({
               partyRole={selectedParty.productRole}
               image={selectedParty.logoUrl}
               infoContainerSx={{
-                display: { xs: "none", md: "flex" },
+                display: { xs: "none", md: "block" },
               }}
             />
           )}
