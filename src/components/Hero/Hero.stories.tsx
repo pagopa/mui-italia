6bfd6a64eafa000c9b65a03289f42a8517b8ea67
Damiano Plebani <damiano.plebani@pagopa.it>
2022-06-08 10:31:34 +0200

    Refactor the new Hero component using native CSS Grid (instead of Grid with Flexbox)

--- src/components/Hero/Hero.stories.tsx
+++ src/components/Hero/Hero.stories.tsx
@@ -1,6 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { Hero } from "@components/Hero/Hero";
-import { position } from "@types";
+import { Hero } from "@components/Hero";
 
 const firstCTA = {
   label: "CallToAction 1",
@@ -28,7 +27,7 @@ export default {
     title: "Title",
     subtitle:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget lacus consequat, accumsan metus sed, pharetra dui. Praesent at accumsan odio. Praesent augue ipsum, pharetra eget metus vel, bibendum dapibus augue. Nunc maximus id eros finibus laoreet. Integer iaculis, neque at feugiat accumsan, nisi magna iaculis nisl, ultricies euismod nulla orci sit amet justo.",
-    textPosition: position.LEFT,
+    inverse: false,
     image: "",
     background: "",
     showPrimary: true,
