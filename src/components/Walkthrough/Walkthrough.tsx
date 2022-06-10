51c9cb89ce6805efaf10a4eec95524f50226897f
Damiano Plebani <damiano.plebani@pagopa.it>
2022-06-10 11:54:18 +0200

    Improve scroll-snap related style, removing extra spacing from mobile version

--- src/components/Walkthrough/Walkthrough.tsx
+++ src/components/Walkthrough/Walkthrough.tsx
@@ -1,3 +1,4 @@
+import { useTheme, useMediaQuery } from "@mui/material";
 import { Box, Container, Stack, Typography } from "@mui/material";
 import { ArrowForward } from "@mui/icons-material";
 
@@ -7,85 +8,111 @@ export interface Item {
   subtitle: string;
 }
 
-export interface ShowcaseProps {
+export interface WalkthroughProps {
   title: string;
   items: Array<Item>;
 }
 
-export const Walkthrough = ({ title, items }: ShowcaseProps) => (
-  <Box bgcolor="#FAFAFA">
-    <Container
-      maxWidth="xl"
-      sx={{
-        py: {
-          xs: 4,
-          sm: 4,
-          md: 8,
-        },
-      }}
-    >
-      <Stack alignContent="center" spacing={8}>
-        <Typography variant="h4" textAlign="center">
-          {title}
-        </Typography>
-        <Box
-          sx={{
-            display: "grid",
-            gap: 3,
-            gridTemplateColumns: "repeat(12, 1fr)",
-          }}
-        >
-          <Box gridColumn="2 / span 10">
-            <Stack
-              direction="row"
-              alignItems="flex-start"
-              justifyContent={{ xs: "flex-start", md: "center" }}
-              spacing={8}
-              width="100%"
-              sx={{
-                overflowX: { xs: "scroll", md: "hidden" },
-                overflowY: "hidden",
-                scrollSnapType: "x mandatory",
+export const Walkthrough = ({ title, items }: WalkthroughProps) => {
+  const theme = useTheme();
+  const isTablet = useMediaQuery(theme.breakpoints.up("md"));
+
+  const containerPadding = {
+    xs: theme.spacing(2),
+    sm: theme.spacing(3),
+    md: 0,
+  };
+
+  return (
+    <Box bgcolor="#FAFAFA">
+      <Container
+        maxWidth="xl"
+        sx={{
+          py: {
+            xs: 4,
+            sm: 4,
+            md: 8,
+          },
+        }}
+        disableGutters={isTablet ? false : true}
+      >
+        <Stack alignContent="center" spacing={{ xs: 4, md: 8 }}>
+          <Typography variant="h4" textAlign="center">
+            {title}
+          </Typography>
+          <Box
+            sx={{
+              display: "grid",
+              gap: 3,
+              gridTemplateColumns: {
+                xs: "repeat(6, minmax(0, 1fr))",
+                md: "repeat(12, minmax(0, 1fr))",
+              },
+            }}
+          >
+            <Box
+              gridColumn={{
+                xs: "span 6",
+                md: "2 / span 10",
               }}
             >
-              {items.map((item, index) => (
-                <Stack
-                  key={index}
-                  alignContent="center"
-                  justifyContent="flex-start"
-                  spacing={4}
-                  sx={{
-                    width: { xs: "80%", md: `calc(100%/${items.length})` },
-                    flex: { xs: "none", md: "auto" },
-                    scrollSnapAlign: "start",
-                    "&:last-child": {
-                      paddingRight: { xs: 8, md: 0 },
-                    },
-                  }}
-                >
-                  <Typography
-                    variant="caption"
-                    color="primary.dark"
-                    alignSelf="flex-start"
+              <Stack
+                direction="row"
+                alignItems="flex-start"
+                justifyContent={{ xs: "flex-start", md: "center" }}
+                spacing={{ xs: 4, lg: 8 }}
+                sx={{
+                  overflowX: { xs: "scroll", md: "hidden" },
+                  overflowY: "hidden",
+                  scrollSnapType: "x mandatory",
+                  scrollPadding: containerPadding,
+                  paddingLeft: containerPadding,
+                  scrollbarWidth: "none",
+                  "&::-webkit-scrollbar": {
+                    display: "none",
+                  },
+                  "&::after": {
+                    content: '""',
+                    paddingLeft: containerPadding,
+                  },
+                }}
+              >
+                {items.map((item, index) => (
+                  <Stack
+                    key={index}
+                    alignContent="center"
+                    justifyContent="flex-start"
+                    spacing={4}
+                    sx={{
+                      minWidth: { xs: "80%", sm: "40%", md: "auto" },
+                      flex: 1,
+                      scrollSnapAlign: "start",
+                    }}
                   >
-                    {(index + 1).toString().padStart(2, "0")}
-                  </Typography>
-                  <Stack direction="row" justifyContent="space-between">
-                    <Box alignSelf="flex-start">{item.icon}</Box>
-                    {index < items.length - 1 && (
-                      <ArrowForward sx={{ color: "primary.dark" }} />
-                    )}
+                    <Typography
+                      variant="caption"
+                      color="primary.dark"
+                      alignSelf="flex-start"
+                    >
+                      {(index + 1).toString().padStart(2, "0")}
+                    </Typography>
+                    <Stack direction="row" justifyContent="space-between">
+                      <Box alignSelf="flex-start">{item.icon}</Box>
+                      {index < items.length - 1 && (
+                        <ArrowForward sx={{ color: "primary.dark" }} />
+                      )}
+                    </Stack>
+                    <Stack spacing={1}>
+                      <Typography variant="h6">{item.title}</Typography>
+                      <Typography variant="body2">{item.subtitle}</Typography>
+                    </Stack>
                   </Stack>
-                  <Stack spacing={1}>
-                    <Typography variant="h6">{item.title}</Typography>
-                    <Typography variant="body2">{item.subtitle}</Typography>
-                  </Stack>
-                </Stack>
-              ))}
-            </Stack>
+                ))}
+              </Stack>
+            </Box>
           </Box>
-        </Box>
-      </Stack>
-    </Container>
-  </Box>
-);
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
