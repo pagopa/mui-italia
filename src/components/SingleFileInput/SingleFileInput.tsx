2138ff6aa356425f6d152bd8b0f299a10c478b84
Carmine Porricelli <carmine.porricelli@pagopa.it>
2022-09-27 20:10:40 +0200

    Added SingleFileInput component

---
+++ src/components/SingleFileInput/SingleFileInput.tsx
@@ -0,0 +1,277 @@
+import { useRef, ChangeEvent, DragEvent, ReactNode, useState } from "react";
+import {
+  Box,
+  CircularProgress,
+  FormControl,
+  FormLabel,
+  IconButton,
+  Input,
+  Typography,
+} from "@mui/material";
+import {
+  AttachFile as AttachFileIcon,
+  Close as CloseIcon,
+  CloudUpload as CloudUploadIcon,
+  Error as ErrorIcon,
+} from "@mui/icons-material";
+import { getContainerStyle, getStatus, verifyAccept } from "./utils";
+
+export type SingleFileInputProps = {
+  /** Input identifier */
+  id: string;
+
+  /** The file to be displayed. */
+  value: File | null;
+
+  /** The label of the input */
+  label?: string;
+
+  /** Sets the error status. */
+  error?: boolean;
+
+  /** The MIME types that the input should accept. */
+  accept?: Array<string>;
+
+  /** Sets the loading status */
+  loading?: boolean;
+
+  /** If enabled, sets the icon and the dropzone label alligned vertically. */
+  vertical?: boolean;
+
+  /** Callback called when the file is selected. */
+  onFileSelected: (file: File) => void;
+
+  /** Callback called when the file is removed. */
+  onFileRemoved?: (file: File) => void;
+
+  /** Callback called when the file is rejected. */
+  onFileRejected?: (file: File) => void;
+
+  /** The label to be displayed in the dropzone. */
+  dropzoneLabel: string;
+
+  /** The label to be displayed above the spinner on loading state. */
+  loadingLabel?: string;
+
+  /**
+   * The label to be displayed when the file is rejected.
+   *
+   * If the label is not provided, the rejected state (on rejected file) will not be displayed.
+   * */
+  rejectedLabel?: string;
+};
+
+const OrientedBox = ({
+  vertical,
+  children,
+}: {
+  vertical: boolean;
+  children: ReactNode;
+}) => (
+  <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    flexDirection={vertical ? "column" : "row"}
+    margin="auto"
+  >
+    {children}
+  </Box>
+);
+
+export enum UploadStatus {
+  IDLE = "IDLE",
+  LOADING = "LOADING",
+  REJECTED = "REJECTED",
+  ERROR = "ERROR",
+  SELECTED = "SELECTED",
+}
+
+export const SingleFileInput = ({
+  id,
+  value,
+  label,
+  error,
+  accept,
+  loading,
+  vertical = false,
+
+  onFileSelected,
+  onFileRemoved,
+  onFileRejected,
+
+  dropzoneLabel,
+  loadingLabel = "Caricamento in corso...",
+  rejectedLabel,
+}: SingleFileInputProps): JSX.Element => {
+  const uploadInputRef = useRef<HTMLButtonElement>();
+
+  const [isFileRejected, setIsFileRejected] = useState(false);
+
+  const status = getStatus(
+    value,
+    !!loading,
+    !!error,
+    isFileRejected && !!rejectedLabel
+  );
+  const containerStyle = getContainerStyle(status);
+
+  const chooseFileHandler = () => {
+    setIsFileRejected(false);
+
+    const target = uploadInputRef.current;
+    target?.click();
+  };
+
+  const handleDragEnter = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setIsFileRejected(false);
+  };
+
+  const handleDragLeave = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDragOver = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    e.dataTransfer.dropEffect = "copy";
+  };
+
+  const handleDrop = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const droppedFile = e.dataTransfer.files[0];
+    if (verifyAccept(droppedFile.type, accept)) {
+      onFileSelected(droppedFile);
+    } else {
+      setIsFileRejected(true);
+      if (onFileRejected) {
+        onFileRejected(droppedFile);
+      }
+    }
+    e.dataTransfer.clearData();
+  };
+
+  const handleSelectFile = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.item(0);
+    if (file) {
+      onFileSelected(file);
+    }
+  };
+
+  const handleRemoveFile = () => {
+    if (value && onFileRemoved) {
+      onFileRemoved(value);
+    }
+  };
+
+  const showDropzone =
+    status === UploadStatus.IDLE ||
+    status === UploadStatus.REJECTED ||
+    status === UploadStatus.ERROR;
+
+  return (
+    <FormControl sx={{ width: "100%" }}>
+      <FormLabel sx={{ fontWeight: 600, mb: 1 }} htmlFor={id}>
+        {label}
+      </FormLabel>
+      <Box
+        sx={{
+          ...containerStyle,
+          padding: "24px",
+          borderRadius: "10px",
+          display: "block",
+          width: "100%",
+          cursor: "pointer",
+        }}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        component="button"
+        onClick={chooseFileHandler}
+        data-testid="loadFromPc"
+      >
+        {showDropzone && (
+          <OrientedBox vertical={vertical}>
+            {status === UploadStatus.REJECTED && (
+              <>
+                <ErrorIcon color="error" sx={{ margin: "0 10px" }} />
+                <Typography color="error" display="inline" variant="body2">
+                  {rejectedLabel}
+                </Typography>
+              </>
+            )}
+
+            {status === UploadStatus.IDLE && (
+              <>
+                <CloudUploadIcon color="primary" sx={{ margin: "0 10px" }} />
+                <Typography display="inline" variant="body2">
+                  {dropzoneLabel}
+                </Typography>
+              </>
+            )}
+
+            {status === UploadStatus.ERROR && (
+              <>
+                <CloudUploadIcon color="error" sx={{ margin: "0 10px" }} />
+                <Typography color="error" display="inline" variant="body2">
+                  {dropzoneLabel}
+                </Typography>
+              </>
+            )}
+
+            <Input
+              inputProps={{ accept }}
+              type="file"
+              id={id}
+              sx={{ display: "none" }}
+              inputRef={uploadInputRef}
+              onChange={handleSelectFile}
+              data-testid="fileInput"
+            />
+          </OrientedBox>
+        )}
+
+        {status === UploadStatus.LOADING && (
+          <OrientedBox vertical={false}>
+            <CircularProgress sx={{ mr: 2 }} />
+
+            <Typography display="inline" variant="body2">
+              {loadingLabel}
+            </Typography>
+          </OrientedBox>
+        )}
+
+        {status === UploadStatus.SELECTED && value && (
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{ width: "100%" }}
+          >
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <AttachFileIcon sx={{ mr: 1 }} color="primary" />
+              <Typography color="primary">{value.name}</Typography>
+              <Typography fontWeight={600} sx={{ marginLeft: "30px" }}>
+                {(value.size / 1024).toFixed(2)}&nbsp;KB
+              </Typography>
+            </Box>
+            {onFileRemoved && (
+              <IconButton onClick={handleRemoveFile}>
+                <CloseIcon />
+              </IconButton>
+            )}
+          </Box>
+        )}
+      </Box>
+    </FormControl>
+  );
+};
+
+export default SingleFileInput;
---
+++ src/components/SingleFileInput/utils.ts
@@ -0,0 +1,92 @@
+import { alpha, SxProps } from "@mui/system";
+import { theme } from "@theme";
+import { UploadStatus } from "./SingleFileInput";
+
+// ellissi 60 caratteri che non copre estensione del file
+
+/**
+ * Check if a mime type matches the set given in accept
+ *
+ * @link https://stackoverflow.com/a/66489392
+ *
+ * @param type the mime type to test, ex image/png
+ * @param accept the mime types to accept, ex audio/*,video/*,image/png
+ * @returns true if the mime is accepted, false otherwise
+ */
+export function verifyAccept(type: string, accept?: Array<string>): boolean {
+  if (!accept) {
+    return true;
+  }
+  return accept.includes(type) || accept.includes(type.split("/")[0] + "/*");
+}
+
+/**
+ * Returns the current status of the SingleFileInput based on his current state
+ *
+ * @param file the current file
+ * @param isLoading if the component is currently loading
+ * @param isFileRejected if the file is rejected
+ * @returns the current SingleFileInput status
+ */
+export function getStatus(
+  file: File | null,
+  isLoading: boolean,
+  error: boolean,
+  isFileRejected: boolean
+): UploadStatus {
+  if (error) {
+    return UploadStatus.ERROR;
+  }
+
+  if (isLoading) {
+    return UploadStatus.LOADING;
+  }
+
+  if (isFileRejected) {
+    return UploadStatus.REJECTED;
+  }
+
+  if (!file) {
+    return UploadStatus.IDLE;
+  }
+
+  return UploadStatus.SELECTED;
+}
+
+/**
+ *
+ * @param status the current status of the SingleFileInput
+ * @returns the associated container styles
+ */
+export function getContainerStyle(status: UploadStatus): SxProps {
+  switch (status) {
+    case UploadStatus.IDLE:
+      return {
+        border: "1px dashed",
+        borderColor: theme.palette.primary.main,
+        backgroundColor: theme.palette.primaryAction.selected,
+      };
+    case UploadStatus.LOADING:
+      return {
+        border: "1px dashed",
+        borderColor: theme.palette.primary.main,
+        backgroundColor: theme.palette.primaryAction.selected,
+        py: 8,
+      };
+    case UploadStatus.REJECTED:
+    case UploadStatus.ERROR:
+      return {
+        border: "1px dashed",
+        borderColor: theme.palette.error.main,
+        backgroundColor: alpha(theme.palette.error.main, 0.1),
+      };
+    case UploadStatus.SELECTED:
+      return {
+        border: "1px solid",
+        borderColor: theme.palette.primary.main,
+        backgroundColor: "white",
+      };
+    default:
+      return {};
+  }
+}
