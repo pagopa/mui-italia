0168fe8fd432188095375d2cef2a2a7d14274f39
Damiano Plebani <damiano.plebani@pagopa.it>
2022-10-11 10:19:46 +0200

    Change decorator dimensions in attempt to avoid large Chromatic snapshots

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -33,8 +33,8 @@ const StoryContainer = ({ children }: StoryContainerProps) => (
     sx={{
       position: "absolute",
       inset: 0,
-      width: "100vw",
-      height: "100vh",
+      width: "100%",
+      height: "100%",
       overflow: "auto",
       padding: "1rem",
       backgroundColor: "background.paper",
