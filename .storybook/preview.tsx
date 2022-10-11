da7c1c45fe4fefec8618d3d865c82616d1203504
Damiano Plebani <damiano.plebani@pagopa.it>
2022-10-11 10:39:41 +0200

    Add `data-chromatic="ignore"` to StoryContainer

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -33,12 +33,13 @@ const StoryContainer = ({ children }: StoryContainerProps) => (
     sx={{
       position: "absolute",
       inset: 0,
-      width: "100%",
-      height: "100%",
+      width: "100vw",
+      height: "100vh",
       overflow: "auto",
       padding: "1rem",
       backgroundColor: "background.paper",
     }}
+    data-chromatic="ignore"
   >
     {children}
   </Box>
