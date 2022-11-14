3be9bb4c2eed54ba8223214f0bc45104a13958ba
Carmine Porricelli <carmine.porricelli@pagopa.it>
2022-11-14 13:07:01 +0100

    Removed useless id generation computations on component render

--- src/components/SingleFileInput/SingleFileInput.tsx
+++ src/components/SingleFileInput/SingleFileInput.tsx
@@ -110,7 +110,7 @@ export const SingleFileInput = ({
 }: SingleFileInputProps): JSX.Element => {
   const uploadInputRef = useRef<HTMLButtonElement>();
 
-  const [id, _] = useState(generateRandomID());
+  const [id, _] = useState(generateRandomID);
   const [isFileRejected, setIsFileRejected] = useState(false);
 
   const status = getStatus(
