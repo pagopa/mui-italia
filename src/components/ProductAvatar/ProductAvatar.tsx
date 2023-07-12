2d64de684487027d58d272361310bf17eacedc4e
loremedda <lorenzo.medda@nttdata.com>
2023-07-12 19:40:27 +0200

    Removed constant and moved logic inside method

--- src/components/ProductAvatar/ProductAvatar.tsx
+++ src/components/ProductAvatar/ProductAvatar.tsx
@@ -39,52 +39,48 @@ export const ProductAvatar = ({
   logoUrl,
   logoBgColor,
   logoAltText,
-}: ProductAvatarProps): JSX.Element => {
-  const paddingValue = size === "small" ? 0.5 : sizesMap[size]?.padding;
-
-  return (
-    <Box
-      id={id}
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        position: "relative",
-        width: sizesMap[size]?.dimension,
-        height: sizesMap[size]?.dimension,
-        backgroundColor: logoBgColor
-          ? logoBgColor
-          : theme.palette.background.paper,
-        boxSizing: "border-box",
-        padding: theme.spacing(paddingValue),
-        borderRadius: theme.spacing(1) /* 8px */,
-        /* Inner shadow */
-        "&:after": {
-          content: "''",
-          position: "absolute",
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0,
-          /* 10% Black inner border if background is set,
+}: ProductAvatarProps): JSX.Element => (
+  <Box
+    id={id}
+    sx={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      position: "relative",
+      width: sizesMap[size]?.dimension,
+      height: sizesMap[size]?.dimension,
+      backgroundColor: logoBgColor
+        ? logoBgColor
+        : theme.palette.background.paper,
+      boxSizing: "border-box",
+      padding: theme.spacing(size === "small" ? 0.5 : sizesMap[size]?.padding),
+      borderRadius: theme.spacing(1) /* 8px */,
+      /* Inner shadow */
+      "&:after": {
+        content: "''",
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        /* 10% Black inner border if background is set,
         otherwise a light grey inner border */
-          boxShadow: logoBgColor
-            ? `inset 0 0 0 1px ${alpha(theme.palette.common.black, 0.1)}`
-            : `inset 0 0 0 1px ${theme.palette.divider}`,
-          borderRadius: "inherit",
-        },
+        boxShadow: logoBgColor
+          ? `inset 0 0 0 1px ${alpha(theme.palette.common.black, 0.1)}`
+          : `inset 0 0 0 1px ${theme.palette.divider}`,
+        borderRadius: "inherit",
+      },
+    }}
+  >
+    <img
+      src={logoUrl}
+      alt={logoAltText}
+      style={{
+        width: "100%",
+        height: "100%",
+        objectFit: "contain",
+        objectPosition: "center",
       }}
-    >
-      <img
-        src={logoUrl}
-        alt={logoAltText}
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "contain",
-          objectPosition: "center",
-        }}
-      />
-    </Box>
-  );
-};
+    />
+  </Box>
+);
