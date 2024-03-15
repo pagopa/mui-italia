c6dbf9d08c46bbf3532bdaacde5c35df1b95d5ea
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2024-03-15 13:59:21 +0100

    Updated prop to pass background color to ribbon

--- src/components/EnvironmentBanner/EnvironmentBanner.tsx
+++ src/components/EnvironmentBanner/EnvironmentBanner.tsx
@@ -3,38 +3,35 @@
 import React from "react";
 import { Alert, Typography } from "@mui/material";
 export interface EnvironmentBannerProps {
-  env: "test" | "prod";
+  bgColor: "warning" | "info";
   message: string;
   icon?: React.ReactNode;
 }
 
 export const EnvironmentBanner = ({
-  env,
+  bgColor,
   message,
   icon,
-}: EnvironmentBannerProps) => {
-  const bgcolor = env === "test" ? "warning.extraLight" : "background.default";
-  return (
-    <Alert
-      sx={{
-        borderLeft: "none",
-        borderRadius: 0,
-        alignItems: "center",
-        justifyContent: "center",
-        bgcolor,
-        "&.MuiPaper-root": {
-          px: { xs: 1, sm: 3 },
-          py: 1,
-        },
-      }}
-      icon={icon ?? false}
+}: EnvironmentBannerProps) => (
+  <Alert
+    sx={{
+      borderLeft: "none",
+      borderRadius: 0,
+      alignItems: "center",
+      justifyContent: "center",
+      bgcolor: `${bgColor}.extraLight`,
+      "&.MuiPaper-root": {
+        px: { xs: 1, sm: 3 },
+        py: 1,
+      },
+    }}
+    icon={icon ?? false}
+  >
+    <Typography
+      variant="body2"
+      sx={{ maxWidth: 480, textAlign: "center", wordBreak: "break-word" }}
     >
-      <Typography
-        variant="body2"
-        sx={{ maxWidth: 480, textAlign: "center", wordBreak: "break-word" }}
-      >
-        {message}
-      </Typography>
-    </Alert>
-  );
-};
+      {message}
+    </Typography>
+  </Alert>
+);
