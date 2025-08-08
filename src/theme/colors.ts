d57fd98f731ccf48eade6dd42a0848b3e62c2f09
Maurizio Flauti <68643346+mflauti@users.noreply.github.com>
2025-08-08 17:08:23 +0200

    feat(M20DS-34): add CodeInput component (#542)

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -3,3 +3,13 @@ export const shadowColor = '#002B55';
 export const backdropBackground = '#17324D';
 export const menuItemBackground = '#17324D';
 export const colorPrimaryContainedHover = '#0055AA'; // Not exposed by the theme object
+export const blue = {
+  500: '#0B3EE3',
+};
+export const error = {
+  600: '#D13333',
+};
+export const neutral = {
+  100: '#E8EBF1',
+  700: '#555C70',
+};
