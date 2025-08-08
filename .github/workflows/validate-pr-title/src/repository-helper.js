073e3ae2d096b9b4cd5fd053647f243f377e895c
AndreaCimini90 <99173401+AndreaCimini90@users.noreply.github.com>
2025-08-08 10:50:26 +0200

    chore(MUI-229): added action to validate pr title (#543)

---
+++ .github/workflows/validate-pr-title/src/repository-helper.js
@@ -0,0 +1,113 @@
+const github = require('@actions/github');
+const core = require('@actions/core');
+const RE2 = require('re2');
+
+async function findReview(octokit) {
+  core.info(`Finding review opened by github-actions[bot] and in status CHANGES_REQUESTED`);
+  try {
+    const prNumber = github.context.payload.pull_request.number;
+    if (!prNumber) {
+      throw new Error(`Failed getting pull request: pr number required`);
+    }
+    const { data: reviews } = await octokit.rest.pulls.listReviews({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: prNumber,
+    });
+    const currentReview = reviews.find(
+      (review) =>
+        review.user.login === 'github-actions[bot]' && review.state === 'CHANGES_REQUESTED'
+    );
+    if (currentReview) {
+      core.info(`Retrieved review with id ${currentReview.id}`);
+    } else {
+      core.info(`No review found`);
+    }
+    return currentReview;
+  } catch (error) {
+    throw new Error(`Failed to get review: ${error}`);
+  }
+}
+
+function validatePullRequestTitle(prTitle, types, scopes) {
+  core.info(`Validate pr title "${prTitle}"`);
+  try {
+    const re = new RE2(`^(?:${types.join('|')})(?:\\(${scopes.join('|')}\\)):\\s(?:.+)$`, 'g');
+    const result = re.test(prTitle);
+    if (result) {
+      core.info(`Pr title "${prTitle}" valid`);
+    } else {
+      core.info(`Pr title "${prTitle}" invalid`);
+    }
+    return result;
+  } catch (error) {
+    throw new Error(`Error in the reqular expression used for validation: ${error}`);
+  }
+}
+
+async function checkPullRequestTitle(octokit, types, scopes) {
+  const prNumber = github.context.payload.pull_request.number;
+  core.info(`Getting pull request with number ${prNumber}`);
+  try {
+    if (!prNumber) {
+      throw new Error(`Failed getting pull request: pr number required`);
+    }
+    const { data: pullRequest } = await octokit.rest.pulls.get({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: prNumber,
+    });
+    core.info(`Pull request with number ${prNumber} retrieved`);
+    const prTitle = pullRequest.title;
+    return validatePullRequestTitle(prTitle, types, scopes);
+  } catch (error) {
+    throw new Error(`Failed getting pull request: ${error}`);
+  }
+}
+
+async function createReview(octokit, reviewBody, status) {
+  const prNumber = github.context.payload.pull_request.number;
+  core.info(`Creating review with status ${status}`);
+  try {
+    if (!prNumber) {
+      throw new Error(`Failed creating review: pr number required`);
+    }
+    const { data: review } = await octokit.rest.pulls.createReview({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: prNumber,
+      event: status,
+      body: reviewBody,
+    });
+    core.info(`Review created with id ${review.id} and status ${status}`);
+  } catch (error) {
+    throw new Error(`Failed creating review: ${error}`);
+  }
+}
+
+async function dismissReview(octokit, id, reviewBody) {
+  const prNumber = github.context.payload.pull_request.number;
+  core.info(`Dismissing review with id ${id}`);
+  try {
+    if (!prNumber) {
+      throw new Error(`Failed dismissing review: pr number required`);
+    }
+    const { data: review } = await octokit.rest.pulls.dismissReview({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: prNumber,
+      review_id: id,
+      message: reviewBody,
+    });
+    core.info(`Review with id ${review.id} dismissed`);
+  } catch (error) {
+    throw new Error(`Failed dismissing review with id ${id}: ${error}`);
+  }
+}
+
+module.exports = {
+  findReview,
+  checkPullRequestTitle,
+  createReview,
+  dismissReview,
+};
