c0e2493f3d9e4241fd81ea5a46b7b7ce9e742551
AndreaCimini90 <99173401+AndreaCimini90@users.noreply.github.com>
2025-08-08 10:50:26 +0200

    chore(MUI-229): added action to validate pr title (#543)

---
+++ .github/workflows/validate-pr-title/src/index.js
@@ -0,0 +1,56 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const path = require('path');
+const fs = require('fs');
+
+const {
+  checkPullRequestTitle,
+  findReview,
+  createReview,
+  dismissReview,
+} = require('./repository-helper');
+const { parseEnum } = require('./input-helper');
+
+async function run() {
+  try {
+    core.debug(`Init octokit client`);
+    // initialize Octokit client
+    const token = core.getInput('token');
+    const types = parseEnum(core.getInput('types'));
+    const scopes = parseEnum(core.getInput('scopes'));
+
+    if (token) {
+      const octokit = github.getOctokit(token);
+      // check if review already exists
+      const review = await findReview(octokit);
+      // check pr title
+      const result = await checkPullRequestTitle(octokit, types, scopes);
+      // 1. create review if it doesn't exist and if the check on pr title fails
+      // 2. if review exists and the check on pr title fails, we mustn't do anything
+      // 3. dismiss review if it exists and the check on pr title succeeds
+      if (!review && !result) {
+        // first read the review template
+        const templatePath = path.join(__dirname, 'review-template.md');
+        const reviewBody = fs.readFileSync(templatePath, 'utf8');
+        // create review
+        await createReview(octokit, reviewBody, 'REQUEST_CHANGES');
+      } else if (review && result) {
+        // dismiss review
+        await dismissReview(
+          octokit,
+          review.id,
+          'The pr title follow the conventional commit specifications'
+        );
+      }
+      if (!result) {
+        throw new Error(`Pr title doesn't follow the conventional commit specifications`);
+      }
+      return;
+    }
+    throw new Error(`No GitHub token specified`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
