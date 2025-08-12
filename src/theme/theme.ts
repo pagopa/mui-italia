5b12363185da18ece2a404229b101b46733478d5
Sarah Donvito <sarah.donvito@pagopa.it>
2025-08-12 13:07:40 +0200

    fix(MUI-230): remove white background in icon button

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -440,7 +440,7 @@ export const theme: Theme = createTheme(foundation, {
       styleOverrides: {
         root: {
           color: foundation.palette.primary.main,
-          backgroundColor: foundation.palette.background.paper,
+          backgroundColor: 'transparent',
           '&:hover': {
             backgroundColor: alpha(foundation.palette.primary.main, 0.08),
           },
