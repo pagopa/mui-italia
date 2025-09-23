009664fc8753550f20065f6203e88d88f1902e09
Andrea Cimini <andrea.cimini@pagopa.it>
2025-09-23 09:34:33 +0200

    set working directory

--- .github/workflows/actions/build/src/index.js
+++ .github/workflows/actions/build/src/index.js
@@ -1,10 +1,12 @@
 import core from '@actions/core';
 import { exec } from '@actions/exec';
+import { resolve } from 'path';
 
 async function run() {
   try {
+    const cwd = resolve(core.getState('repositoryPath'));
     // install yarn
-    await exec('yarn', ['install', 'immutable'], { silent: true, cwd: '.' });
+    await exec('yarn', ['install', 'immutable'], { silent: true, cwd });
   } catch (error) {
     core.setFailed(error.message);
   }
