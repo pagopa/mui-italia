1bcde92a1121b0981805a542068f82072ad17d8c
Andrea Cimini <andrea.cimini@pagopa.it>
2025-09-23 10:23:00 +0200

    not install node

--- .github/workflows/actions/build/src/index.js
+++ .github/workflows/actions/build/src/index.js
@@ -7,7 +7,7 @@ async function run() {
     const cwd = resolve(core.getState('repositoryPath'));
     core.info(cwd);
     // install yarn
-    // await exec('yarn', ['install', 'immutable'], { silent: true, cwd });
+    await exec('yarn', ['install', 'immutable'], { silent: true, cwd });
   } catch (error) {
     core.setFailed(error.message);
   }
