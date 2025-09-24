641e5d6210cade217395a904d17e10618601d850
Andrea Cimini <andrea.cimini@pagopa.it>
2025-09-24 18:47:29 +0200

    fixed yarn install command

--- .github/workflows/actions/build/src/index.js
+++ .github/workflows/actions/build/src/index.js
@@ -6,7 +6,7 @@ async function run() {
   try {
     const cwd = resolve(core.getState('repositoryPath'));
     // install yarn
-    await exec('yarn', ['install', 'immutable'], { silent: true, cwd });
+    await exec('yarn', ['install', '--immutable'], { silent: true, cwd });
   } catch (error) {
     core.setFailed(error.message);
   }
