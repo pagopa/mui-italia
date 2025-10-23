b9fba8928e9676c21cc8be357278cbd13887a4c9
Sarah Donvito <sarah.donvito@pagopa.it>
2025-10-23 15:38:38 +0200

    M20DS-2: update colors file

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,4 +1,4 @@
-import { hexToRgb, rgbToHex } from '@mui/system';
+import { hexToRgb } from '@mui/system';
 
 export const colorTextPrimary = '#17324D';
 export const shadowColor = '#002B55';
@@ -6,16 +6,19 @@ export const backdropBackground = '#17324D';
 export const menuItemBackground = '#17324D';
 export const colorPrimaryContainedHover = '#0055AA'; // Not exposed by the theme object
 
+// base palette
 export const neutral = {
   black: '#0E0F13',
-  850: '#2B2E38',
-  700: '#555C70',
-  650: '#636B82',
-  450: '#99A3C1',
-  300: '#BBC2D6',
-  200: '#D2D6E3',
-  100: '#E8EBF1',
-  50: '#F4F5F8',
+  grey: {
+    850: '#2B2E38',
+    700: '#555C70',
+    650: '#636B82',
+    450: '#99A3C1',
+    300: '#BBC2D6',
+    200: '#D2D6E3',
+    100: '#E8EBF1',
+    50: '#F4F5F8',
+  },
   white: '#FFFFFF',
 };
 
@@ -89,28 +92,95 @@ export const purple = {
   50: '#EEE8FC',
 };
 
+// elements colors
 export const opacity = {
   black: {
     20: `rgba(${hexToRgb(neutral.black)}, 0.2)`, // #0E0F13 · 20%
-    60: '#0f101599', // #0E0F13 · 60%
+    60: `rgba(${hexToRgb(neutral.black)}, 0.6)`, // #0E0F13 · 60%
   },
   blue: {
-    8: '#0b3ee314', // #0B3EE3 · 8%
-    20: '#0b3ee333', // #0B3EE3 · 20%
+    8: `rgba(${hexToRgb(blue[500])}, 0.08)`, // #0B3EE3 · 8%
+    20: `rgba(${hexToRgb(blue[500])}, 0.2)`, // #0B3EE3 · 20%
   },
 };
 
 export const text = {
   heading: neutral.black,
-  description: neutral[700],
+  description: neutral.grey[700],
 };
 
 export const surface = {
   primary: neutral.white,
   overlay: opacity.black[20],
   blue: blue[500],
-  skeleton: neutral[100],
-  skeletonOpacity: neutral[450],
-  placeholder: neutral[50],
+  skeleton: neutral.grey[100],
+  skeletonOpacity: neutral.grey[450],
+  placeholder: neutral.grey[50],
   blueItalia: blueitalia[50],
+};
+
+export const icon = {
+  active: neutral.black,
+  decorative: neutral.grey[300],
+  action: blue[500],
+  success: success[700],
+  info: info[700],
+  warning: warning[700],
+  error: error[600],
+  button: opacity.black[60],
+};
+
+export const divider = {
+  default: neutral.grey[100],
+};
+
+export const badge = {
+  default: {
+    filled: { background: blue[50], text: blue[850] },
+    outlined: { background: blue[600], text: {} },
+  },
+  warning: {
+    filled: { background: warning[100], text: warning[850] },
+    outlined: { background: warning[850], text: {} },
+  },
+  success: {
+    filled: { background: success[100], text: success[850] },
+    outlined: { background: success[850], text: {} },
+  },
+  error: {
+    filled: { background: error[100], text: error[850] },
+    outlined: { background: error[600], text: {} },
+  },
+  highlighted: {
+    filled: { background: turquoise[50], text: turquoise[850] },
+    outlined: { background: turquoise[850], text: {} },
+  },
+};
+
+export const progress = {
+  track: blue[500],
+  total: neutral.white,
+};
+
+export const button = {
+  primary: {
+    default: { background: blueitalia[500], text: neutral.white },
+    hover: { background: blueitalia[600], text: neutral.white },
+    disabled: { background: neutral.grey[200], text: neutral.grey[700] },
+    contrast: {
+      default: { background: neutral.white, text: neutral.grey[700] },
+      hover: { background: neutral.grey[50], text: neutral.grey[700] },
+      disabled: { background: blue[200], text: neutral.grey[300] },
+    },
+  },
+  secondary: {
+    default: { background: {}, text: {} },
+    hover: { background: {}, text: {} },
+    disabled: { background: {}, text: {} },
+  },
+  tertiary: {
+    default: { background: {}, text: {} },
+    hover: { background: {}, text: {} },
+    disabled: { background: {}, text: {} },
+  },
 };
