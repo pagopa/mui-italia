471c8494fba32197c89103570cbfd29233ee9f59
AndreaCimini90 <99173401+AndreaCimini90@users.noreply.github.com>
2025-12-02 13:26:18 +0100

    fix(M20DS-45): fix layout, hide clera icon after input clear and make emptyState configurable (#607)

--- src/stories/CodeInput.stories.tsx
+++ src/stories/CodeInput.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import CodeInput, { CodeInputProps } from '../components/CodeInput/CodeInput';
 
