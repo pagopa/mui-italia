86f488bf2b3980a84c05f70e065eb4ec1d26832c
AndreaCimini90 <99173401+AndreaCimini90@users.noreply.github.com>
2026-01-15 11:39:08 +0100

    fix(M20DS-10): added variant default to tag group component (#630)

--- src/components/TagGroup/TagGroup.tsx
+++ src/components/TagGroup/TagGroup.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import React from "react";
+import React from 'react';
 
 // Components
-import { Stack } from "@mui/material";
-import { Tag } from "@components/Tag";
+import { Stack } from '@mui/material';
+import { Tag } from '@components/Tag';
 
 export interface TagGroupProps {
   children: JSX.Element | Array<JSX.Element>;
@@ -12,20 +12,17 @@ export interface TagGroupProps {
   tagRef?: React.Ref<HTMLButtonElement>;
 }
 
-export const TagGroup = ({
-  children,
-  visibleItems = NaN,
-}: TagGroupProps): JSX.Element => (
+export const TagGroup = ({ children, visibleItems = NaN }: TagGroupProps): JSX.Element => (
   <Stack
     spacing={0.5}
     direction="row"
     justifyContent="flex-start"
     alignItems="flex-start"
     sx={(theme) => ({
-      flexWrap: "wrap",
+      flexWrap: 'wrap',
       mt: -0.5,
       ml: -0.5,
-      "& > *": { marginTop: `${theme.spacing(0.5)} !important`, ml: 0.5 },
+      '& > *': { marginTop: `${theme.spacing(0.5)} !important`, ml: 0.5 },
     })}
   >
     {/* If visibleItems is not set, show all children items.
@@ -35,7 +32,7 @@ export const TagGroup = ({
           i < visibleItems
             ? child
             : visibleItems === i && (
-                <Tag value={`+${React.Children.count(children) - i}`} />
+                <Tag value={`+${React.Children.count(children) - i}`} variant="default" />
               )
         )
       : children}
