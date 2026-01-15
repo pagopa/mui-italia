d46ab44b6b4c5ba62b4b8c12651f809a8552c116
Sarah Donvito <73442810+SarahDonvito@users.noreply.github.com>
2026-01-15 09:17:10 +0100

    feat(M20DS-10): update Tag element (#621)

--- src/components/Tag/Tag.stories.tsx
+++ src/components/Tag/Tag.stories.tsx
@@ -1,17 +1,54 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import StarRoundedIcon from '@mui/icons-material/StarRounded';
+import AttachFileRoundedIcon from '@mui/icons-material/AttachFileRounded';
 
-import { Tag } from "./Tag";
+import { Tag } from './Tag';
 
 export default {
-  title: "Components/Tag",
+  title: 'Components/Tag',
   component: Tag,
   args: {
-    value: "Tag Content",
-    variant: "default",
-    color: "default",
+    value: 'Tag Content',
+    variant: 'default',
+    ariaLabel: 'Stato: test',
   },
 } as ComponentMeta<typeof Tag>;
 
 const Template: ComponentStory<typeof Tag> = (args) => <Tag {...args} />;
 
-export const Default = Template.bind({});
+export const DefaultTag = Template.bind({});
+DefaultTag.args = {
+  variant: 'default',
+  icon: StarRoundedIcon,
+};
+
+export const InfoTag = Template.bind({});
+InfoTag.args = {
+  variant: 'info',
+};
+
+export const WarningTag = Template.bind({});
+WarningTag.args = {
+  variant: 'warning',
+};
+
+export const ErrorTag = Template.bind({});
+ErrorTag.args = {
+  variant: 'error',
+};
+
+export const SuccessTag = Template.bind({});
+SuccessTag.args = {
+  variant: 'success',
+};
+
+export const NoIconTag = Template.bind({});
+NoIconTag.args = {
+  icon: undefined,
+};
+
+export const OnlyIconTag = Template.bind({});
+OnlyIconTag.args = {
+  variant: 'only-icon',
+  icon: AttachFileRoundedIcon,
+};
