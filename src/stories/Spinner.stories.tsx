cbe1d88178455b2e6da245f5053ae0d80ffa888c
martinaCampoli <martina.campoli@pagopa.it>
2026-03-31 09:53:35 +0200

    feat: add custom theme for Spinner component and stories

---
+++ src/stories/Spinner.stories.tsx
@@ -0,0 +1,31 @@
+import { StoryFn, Meta } from '@storybook/react-vite';
+
+import { CircularProgress } from '@mui/material';
+
+export default {
+  title: 'MUI Components/Spinner',
+  component: CircularProgress,
+  argTypes: {
+    table: {
+      disable: true,
+    },
+  },
+} as Meta<typeof CircularProgress>;
+
+const Template: StoryFn<typeof CircularProgress> = (args) => <CircularProgress {...args} />;
+
+export const Default = Template.bind({});
+Default.args = { color: 'primary' };
+
+export const Secondary = Template.bind({});
+Secondary.args = { color: 'secondary' };
+Secondary.decorators = [
+  (Story) => (
+    <div style={{ padding: '1rem', backgroundColor: '#000' }}>
+      <Story />
+    </div>
+  ),
+];
+
+export const Error = Template.bind({});
+Error.args = { color: 'error' };
