f0018c2698f3cbc71fb080b61c5ca0fca587a645
martinaCampoli <martina.campoli@pagopa.it>
2026-03-31 12:15:44 +0200

    feat: remove unused Spinner stories and update CircularProgress color overrides

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -117,6 +117,17 @@ declare module '@mui/material/Button' {
   }
 }
 
+declare module '@mui/material/CircularProgress' {
+  interface CircularProgressPropsColorOverrides {
+    primary: true;
+    secondary: false;
+    error: false;
+    warning: false;
+    info: false;
+    success: false;
+  }
+}
+
 declare module '@mui/material/IconButton' {
   interface IconButtonPropsColorOverrides {
     secondary: false;
@@ -463,9 +474,6 @@ export const theme: Theme = createTheme(foundation, {
         },
         colorPrimary: {
           color: blue[500],
-        },
-        colorSecondary: {
-          color: foundation.palette.common.white,
         },
         colorError: {
           color: error[600],
