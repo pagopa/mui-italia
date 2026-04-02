560501f3ecd629e2fb7e28c1f5a73d83e0418109
Sarah Donvito <sarah.donvito@pagopa.it>
2026-04-02 20:21:26 +0200

    add new illustrations

--- src/illustrations/Illustrations.stories.tsx
+++ src/illustrations/Illustrations.stories.tsx
@@ -28,7 +28,29 @@ import {
   IllusMIEmailValidation,
   IllusMIQuick,
   IllusMICompleted,
+  IllusMIMaintenance,
+  IllusMISavingMoney,
+  IllusMIDataSecurity,
+  IllusMIError,
+  IllusMIMultipleFiles,
+  IllusMINews,
+  IllusMISimplify,
+  IllusMIAlarmClock,
+  IllusMIIdea,
+  IllusMIInstitution,
+  IllusMISafeDeposit,
+  IllusMIBell,
+  IllusMIEnvelope,
+  IllusMILeggi,
+  IllusMIPush,
+  IllusMIUploadFile,
+  IllusMIAssistance,
+  IllusMICode,
+  IllusMIFileCheck,
+  IllusMIFlag,
+  IllusMIUserManual,
 } from './';
+import { JSX } from 'react';
 
 export interface IllusBoxProps {
   illustration: JSX.Element;
@@ -62,7 +84,9 @@ const IllusBox = ({ name, illustration }: IllusBoxProps): JSX.Element => (
 
 export const Overview: StoryFn<typeof Box> = () => (
   <>
-    <Typography variant="h5">Mui Italia 2.0 Illustrations</Typography>
+    <Typography variant="h5" mb={2}>
+      Mui Italia 2.0 Illustrations
+    </Typography>
     <Box
       sx={{
         display: 'grid',
@@ -75,9 +99,32 @@ export const Overview: StoryFn<typeof Box> = () => (
       <IllusBox name={'<IllusMIEmailValidation />'} illustration={<IllusMIEmailValidation />} />
       <IllusBox name={'<IllusMIQuick />'} illustration={<IllusMIQuick />} />
       <IllusBox name={'<IllusMICompleted />'} illustration={<IllusMICompleted />} />
+      <IllusBox name={'<IllusMIMaintenance />'} illustration={<IllusMIMaintenance />} />
+      <IllusBox name={'<IllusMISavingMoney />'} illustration={<IllusMISavingMoney />} />
+      <IllusBox name={'<IllusMISimplify />'} illustration={<IllusMISimplify />} />
+      <IllusBox name={'<IllusMIError />'} illustration={<IllusMIError />} />
+      <IllusBox name={'<IllusMIDataSecurity />'} illustration={<IllusMIDataSecurity />} />
+      <IllusBox name={'<IllusMINews />'} illustration={<IllusMINews />} />
+      <IllusBox name={'<IllusMIMultipleFiles />'} illustration={<IllusMIMultipleFiles />} />
+      <IllusBox name={'<IllusMIIdea />'} illustration={<IllusMIIdea />} />
+      <IllusBox name={'<IllusMIInstitution />'} illustration={<IllusMIInstitution />} />
+      <IllusBox name={'<IllusMISafeDeposit />'} illustration={<IllusMISafeDeposit />} />
+      <IllusBox name={'<IllusMIAlarmClock />'} illustration={<IllusMIAlarmClock />} />
+      <IllusBox name={'<IllusMIEnvelope />'} illustration={<IllusMIEnvelope />} />
+      <IllusBox name={'<IllusMIBell />'} illustration={<IllusMIBell />} />
+      <IllusBox name={'<IllusMIUploadFile />'} illustration={<IllusMIUploadFile />} />
+      <IllusBox name={'<IllusMIPush />'} illustration={<IllusMIPush />} />
+      <IllusBox name={'<IllusMILeggi />'} illustration={<IllusMILeggi />} />
+      <IllusBox name={'<IllusMICode />'} illustration={<IllusMICode />} />
+      <IllusBox name={'<IllusMIFileCheck />'} illustration={<IllusMIFileCheck />} />
+      <IllusBox name={'<IllusMIAssistance />'} illustration={<IllusMIAssistance />} />
+      <IllusBox name={'<IllusMIFlag />'} illustration={<IllusMIFlag />} />
+      <IllusBox name={'<IllusMIUserManual />'} illustration={<IllusMIUserManual />} />
     </Box>
     <Divider sx={{ my: 4 }} />
-    <Typography variant="h5">Mui Italia Illustrations</Typography>
+    <Typography variant="h5" mb={2}>
+      Mui Italia Illustrations
+    </Typography>
     <Box
       sx={{
         display: 'grid',
