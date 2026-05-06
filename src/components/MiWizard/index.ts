d8b85f2ccddd51ed2f77d94eef73b60b21d4af4b
Gaetano Amato <gaetano.amato@pagopa.it>
2026-05-06 16:30:38 +0200

    fix: export MIWizard components from package entrypoints

--- src/components/MiWizard/index.ts
+++ src/components/MiWizard/index.ts
@@ -1,3 +1,8 @@
+export { default as MIWizard } from './MIWizard';
 export * from './MIWizard';
+
+export { default as MIWizardStep } from './MIWizardStep';
 export * from './MIWizardStep';
+
+export { default as MIWizardStepper } from './MIWizardStepper';
 export * from './MIWizardStepper';
