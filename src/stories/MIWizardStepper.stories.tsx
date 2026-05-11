fdb3767bc3c8f2f90f86ab465905f54b4accd131
Gaetano Amato <gaetano.amato@pagopa.it>
2026-05-11 12:15:07 +0200

    Add MIWizardStepper stories

--- src/stories/MIWizardStepper.stories.tsx
+++ src/stories/MIWizardStepper.stories.tsx
@@ -1,6 +1,6 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import { StoryObj, Meta } from '@storybook/react-vite';
 
-import MIWizardStepper from '../components/MIWizard/MIWizardStepper';
+import { MIWizardStepper } from '../components/MIWizard';
 
 const meta = {
   title: 'Debug/MIWizardStepperDebug',
