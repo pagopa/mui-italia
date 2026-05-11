dc17caf3a946589a481bc78aa290169fb5def5d7
Gaetano Amato <gaetano.amato@pagopa.it>
2026-05-11 10:29:57 +0200

    Add MIWizardStepper stories

--- src/stories/MIWizardStepper.stories.tsx
+++ src/stories/MIWizardStepper.stories.tsx
@@ -1,37 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Box } from '@mui/material';
-import { breakpointsChromaticValues } from '@theme';
-
 import MIWizardStepper from '@components/MIWizard/MIWizardStepper';
-
-const componentMaxWidth = 900;
-
-const steps = [{ label: 'Personal data' }, { label: 'Review' }, { label: 'Confirmation' }];
 
 const meta = {
   title: 'MUI Components/Navigation/MIWizardStepper',
   component: MIWizardStepper,
-  parameters: {
-    chromatic: {
-      viewports: breakpointsChromaticValues.filter((resolution) => resolution <= componentMaxWidth),
-    },
-  },
-  decorators: [
-    (Story) => (
-      <Box sx={{ p: 2, pb: 4, boxSizing: 'border-box', width: '100%' }}>
-        <Box sx={{ maxWidth: '100%', mx: 'auto' }}>
-          <Story />
-        </Box>
-      </Box>
-    ),
-  ],
-  argTypes: {
-    activeStep: {
-      control: { type: 'number', min: 0, max: steps.length - 1 },
-    },
-  },
-} satisfies Meta<React.ComponentProps<typeof MIWizardStepper>>;
+} satisfies Meta<typeof MIWizardStepper>;
 
 export default meta;
 
@@ -39,32 +13,7 @@ type Story = StoryObj<typeof meta>;
 
 export const FirstStep: Story = {
   args: {
-    steps,
+    steps: [{ label: 'Personal data' }, { label: 'Review' }, { label: 'Confirmation' }],
     activeStep: 0,
-  },
-};
-
-export const MiddleStep: Story = {
-  args: {
-    steps,
-    activeStep: 1,
-  },
-};
-
-export const LastStep: Story = {
-  args: {
-    steps,
-    activeStep: 2,
-  },
-};
-
-export const LongLabels: Story = {
-  args: {
-    activeStep: 1,
-    steps: [
-      { label: 'Insert personal and contact information' },
-      { label: 'Review all entered information before submitting' },
-      { label: 'Confirm and complete the wizard flow' },
-    ],
   },
 };
