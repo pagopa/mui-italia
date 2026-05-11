73cb211b3075f9ec1b522723b2f6b0d0b2b1a532
Gaetano Amato <gaetano.amato@pagopa.it>
2026-05-11 14:18:38 +0200

    update MIWizardSteppe.stories

--- src/components/MIWizard/index.ts
+++ src/components/MIWizard/index.ts
@@ -1,3 +1,6 @@
+export { default as MIWizard } from './MIWizard';
 export * from './MIWizard';
+export { default as MIWizardStep } from './MIWizardStep';
 export * from './MIWizardStep';
 export { default as MIWizardStepper } from './MIWizardStepper';
+export * from './MIWizardStepper';
--- src/stories/MIWizardStepper.stories.tsx
+++ src/stories/MIWizardStepper.stories.tsx
@@ -1,20 +1,38 @@
-import { StoryObj, Meta } from '@storybook/react-vite';
+import { Meta, StoryObj } from '@storybook/react-vite';
 
 import { MIWizardStepper } from '@components/MIWizard';
 
 const meta = {
-  title: 'Debug/MIWizardStepperDebug',
+  title: 'MUI Components/Navigation/MIWizardStepper',
 } satisfies Meta;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Basic: Story = {
+const steps = [{ label: 'Personal data' }, { label: 'Review' }, { label: 'Confirmation' }];
+
+export const FirstStep: Story = {
+  render: () => <MIWizardStepper steps={steps} activeStep={0} />,
+};
+
+export const MiddleStep: Story = {
+  render: () => <MIWizardStepper steps={steps} activeStep={1} />,
+};
+
+export const LastStep: Story = {
+  render: () => <MIWizardStepper steps={steps} activeStep={2} />,
+};
+
+export const LongLabels: Story = {
   render: () => (
     <MIWizardStepper
-      steps={[{ label: 'Personal data' }, { label: 'Review' }, { label: 'Confirmation' }]}
-      activeStep={0}
+      activeStep={1}
+      steps={[
+        { label: 'Insert personal and contact information' },
+        { label: 'Review all entered information before submitting' },
+        { label: 'Confirm and complete the wizard flow' },
+      ]}
     />
   ),
 };
