e5a7018d46e30ac2351da0da68ceab1a99e54d66
Andrea Cimini <andrea.cimini@pagopa.it>
2026-06-04 15:59:36 +0200

    migrated to dist also for release action

--- .github/workflows/actions/release/src/utility-helper.js
+++ .github/workflows/actions/release/src/utility-helper.js
@@ -1,4 +1,3 @@
-import RE2 from 're2';
 import { info } from '@actions/core';
 import { Bumper } from 'conventional-recommended-bump';
 
@@ -9,7 +8,7 @@ export function toSentenceCase(word) {
 }
 
 export function isTagOrBranch(ref) {
-  const tagRegex = new RE2(/^v(\d+)\.(\d+)\.(\d+)(?:-RC|-rc\.(\d+))?$/);
+  const tagRegex = new RegExp(/^v(\d+)\.(\d+)\.(\d+)(?:-RC|-rc\.(\d+))?$/);
   if (ref.match(tagRegex)) {
     return 'tag';
   }
@@ -19,7 +18,7 @@ export function isTagOrBranch(ref) {
 export async function calcNextTag(latestTag, type, finalRelease) {
   info(`Calculating next tag starting from ${latestTag}`);
   try {
-    const rcRegex = new RE2(/^v(\d+)\.(\d+)\.(\d+)(?:(?:-RC|-rc)\.(\d+))?$/);
+    const rcRegex = new RegExp(/^v(\d+)\.(\d+)\.(\d+)(?:(?:-RC|-rc)\.(\d+))?$/);
     const match = latestTag.match(rcRegex);
     // match returns an array in which the first element is the string itslef
     // the second element is the major, the third is the minor and the fourth is the path
