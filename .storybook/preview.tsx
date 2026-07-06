a09e9cc5104a05a81b201b720fe854413ce6ccac
AndreaCimini90 <99173401+AndreaCimini90@users.noreply.github.com>
2026-07-06 17:29:03 +0200

    docs(MUI-249): added documentation for newest components (#711)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Decorator } from '@storybook/react-vite';
+import { Decorator, Preview } from '@storybook/react-vite';
 
 /* Storybook Theme */
 import { sbTheme } from './theme';
@@ -8,37 +8,47 @@ import { ThemeProvider, Box } from '@mui/material';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
 /* MUI Italia Theme */
-import { theme as lightTheme, darkTheme } from '../src/theme';
+import { theme as lightTheme, darkTheme, theme } from '../src/theme';
 
 /* MUI Italia Theme-Next */
 import { themeNext } from '../src/theme/themeNext';
 
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  layout: 'fullscreen',
-  controls: {
-    expanded: true,
-    matchers: {
-      /* color: /(background|color)$/i, */
-      date: /Date$/,
-    },
+const backgrounds = {
+  default: {
+    name: 'default',
+    value: theme.colors.blue[100],
   },
-  docs: {
-    sbTheme,
-  },
-};
+} as const;
 
-interface StoryContainerProps {
+type BackgroundKey = keyof typeof backgrounds;
+
+const StoryContainer = ({
+  children,
+  backgroundKey,
+}: {
   children: React.ReactNode;
-}
-
-const StoryContainer = ({ children }: StoryContainerProps) => (
-  <Box sx={{ backgroundColor: 'background.paper', padding: '1rem' }} data-chromatic="ignore">
+  backgroundKey: BackgroundKey;
+}) => (
+  <Box
+    sx={{
+      backgroundColor: backgrounds[backgroundKey].value,
+      p: {
+        xs: 3,
+        md: 5,
+      },
+      boxSizing: 'border-box',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 3,
+    }}
+    data-chromatic="ignore"
+  >
     {children}
   </Box>
 );
 
-export const withTheme: Decorator = (Story, context) => {
+const withTheme: Decorator = (Story, context) => {
   // More info about this decorator
   // https://storybook.js.org/blog/how-to-add-a-theme-switcher-to-storybook/
 
@@ -51,11 +61,14 @@ export const withTheme: Decorator = (Story, context) => {
     [prefersDarkMode]
   );
 
+  const selectedBackground =
+    context.parameters.backgroundKey ?? context.globals.backgrounds?.value ?? 'neutral';
+
   switch (selectedTheme) {
     case 'system': {
       return (
         <ThemeProvider theme={currentTheme}>
-          <StoryContainer>
+          <StoryContainer backgroundKey={selectedBackground}>
             <Story />
           </StoryContainer>
         </ThemeProvider>
@@ -65,7 +78,7 @@ export const withTheme: Decorator = (Story, context) => {
     case 'dark': {
       return (
         <ThemeProvider theme={darkTheme}>
-          <StoryContainer>
+          <StoryContainer backgroundKey={selectedBackground}>
             <Story />
           </StoryContainer>
         </ThemeProvider>
@@ -74,7 +87,7 @@ export const withTheme: Decorator = (Story, context) => {
     case 'next': {
       return (
         <ThemeProvider theme={themeNext}>
-          <StoryContainer>
+          <StoryContainer backgroundKey={selectedBackground}>
             <Story />
           </StoryContainer>
         </ThemeProvider>
@@ -85,7 +98,7 @@ export const withTheme: Decorator = (Story, context) => {
     default: {
       return (
         <ThemeProvider theme={lightTheme}>
-          <StoryContainer>
+          <StoryContainer backgroundKey={selectedBackground}>
             <Story />
           </StoryContainer>
         </ThemeProvider>
@@ -94,25 +107,53 @@ export const withTheme: Decorator = (Story, context) => {
   }
 };
 
-export const globalTypes = {
-  theme: {
-    name: 'Theme',
-    description: 'Global theme for components',
-    defaultValue: 'light',
-    toolbar: {
-      // The icon for the toolbar item
-      icon: 'circlehollow',
-      title: 'Theme',
-      // Array of options
-      items: [
-        { value: 'system', icon: 'cog', title: 'System' },
-        { value: 'light', icon: 'circlehollow', title: 'Light' },
-        { value: 'dark', icon: 'circle', title: 'Dark' },
-        { value: 'next', icon: 'paintbrush', title: 'Theme Next' },
-      ],
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    layout: 'fullscreen',
+    controls: {
+      expanded: true,
+      matchers: {
+        /* color: /(background|color)$/i, */
+        date: /Date$/,
+      },
+    },
+    docs: {
+      theme: sbTheme,
+    },
+    backgrounds: {
+      options: backgrounds,
+    },
+    options: {
+      storySort: {
+        order: ['Foundation', ['Colors', 'Breakpoints'], 'Components', 'MUI Components'],
+      },
     },
   },
+
+  globalTypes: {
+    theme: {
+      name: 'Theme',
+      description: 'Global theme for components',
+      defaultValue: 'light',
+      toolbar: {
+        // The icon for the toolbar item
+        icon: 'circlehollow',
+        title: 'Theme',
+        // Array of options
+        items: [
+          { value: 'system', icon: 'cog', title: 'System' },
+          { value: 'light', icon: 'circlehollow', title: 'Light' },
+          { value: 'dark', icon: 'circle', title: 'Dark' },
+          { value: 'next', icon: 'paintbrush', title: 'Theme Next' },
+        ],
+      },
+    },
+  },
+
+  decorators: [withTheme],
+
+  tags: ['autodocs'],
 };
 
-export const decorators = [withTheme];
-export const tags = ['autodocs'];
+export default preview;
