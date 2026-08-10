c7577ea78e7712b8a2fd0fc716920d8130c36541
antofo <antonio.forte@digitouch.it>
2026-08-10 14:57:04 +0200

    feat: adjust MISwitch to restrict customization (M20DS-28)

--- src/components/MISwitch/MISwitch.tsx
+++ src/components/MISwitch/MISwitch.tsx
@@ -1,28 +1,29 @@
 import MuiSwitch, { SwitchProps } from '@mui/material/Switch';
 import { styled } from '@mui/material/styles';
 
-interface MISwitchProps extends SwitchProps {}
+type MISwitchProps = Omit<
+  SwitchProps,
+  'color' | 'size' | 'classes' | 'className' | 'style' | 'focusVisibleClassName' | 'sx'
+>;
 
 const StyledSwitch = styled(MuiSwitch)(({ theme }) => ({
-  '& .MuiSwitch-colorPrimary': {
-    '&.MuiSwitch-switchBase': {
-      '& + .MuiSwitch-track': {
-        backgroundColor: theme.colors.neutral.grey[700], // '#555C70',
+  '& .MuiSwitch-switchBase': {
+    '& + .MuiSwitch-track': {
+      backgroundColor: theme.colors.neutral.grey[700], // '#555C70',
+    },
+    '&.Mui-focusVisible': {
+      '& .MuiSwitch-thumb': {
+        color: theme.colors.neutral.grey[700],
       },
+    },
+    '&.Mui-checked': {
       '&.Mui-focusVisible': {
         '& .MuiSwitch-thumb': {
-          color: theme.colors.neutral.grey[700],
+          color: theme.colors.blue[500],
         },
       },
-      '&.Mui-checked': {
-        '&.Mui-focusVisible': {
-          '& .MuiSwitch-thumb': {
-            color: theme.colors.blue[500],
-          },
-        },
-        '& + .MuiSwitch-track': {
-          backgroundColor: theme.colors.blue[500], // '#555C70',
-        },
+      '& + .MuiSwitch-track': {
+        backgroundColor: theme.colors.blue[500], // '#555C70',
       },
     },
   },
