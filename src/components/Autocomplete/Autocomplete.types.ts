9203b21042127bd8e7847574ed85365f78713831
alten-dturus <davide.turus@alten.it>
2026-08-31 14:23:46 +0200

    fix(M20DS-138): fix type AutocompleteValue

--- src/components/Autocomplete/Autocomplete.types.ts
+++ src/components/Autocomplete/Autocomplete.types.ts
@@ -53,7 +53,7 @@ export type InputChangeReason = 'input' | 'clear' | 'selectOption';
 
 // if multiple is true, value is an array of T
 // otherwise we have a single element
-export type AutocompleteValue<T, Multiple> = Multiple extends true ? Array<T> : T;
+export type AutocompleteValue<T, Multiple> = Multiple extends true ? Array<T> : T | null;
 
 export interface AutocompleteProps<T, Multiple extends boolean | undefined>
   extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> {
